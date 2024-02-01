797d78da47aac5625499e6b4ba08187ec35dfeb4
subash-0 <subash67335@gmail.com>
2024-02-01 23:25:00 +0545

    created page and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 
 import React from 'react'
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {About , Home , SignIn, SignUp, Profile} from "./pages";
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}  />
+        <Route path='/sign-in' element={<SignIn />}  />
+        <Route path='/sign-up' element={<SignUp />}  />
+        <Route path='/profile' element={<Profile />}  />
+        <Route path='/about' element={<About />}  />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Home} from "./Home";
+export { default as About} from "./About";
+export { default as SignIn} from "./SignIn";
+export { default as SignUp} from "./Home";
+export { default as Profile} from "./Profile";
