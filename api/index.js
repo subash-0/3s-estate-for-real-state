0198aff0d18ad98ef8da114319a1c19426c238b5
subash-0 <subash67335@gmail.com>
2024-02-02 22:13:39 +0545

    test api added

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import  express  from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
 const app = express();
-require("./db/db");
+import "./db/db.js";
 dotenv.config();
+app.use(cors({
+    credentials:true,
+    
+}));
+app.use(cookieParser());
+
 const PORT = process.env.PORT;
+ //Routes 
+ import userRoute from "./db/routers/user.router.js";
 
 
-
+app.use("/api/v1",userRoute);
 app.listen(PORT,()=>{
     console.log(`Server is running at port: ${PORT} !!`)
 })
