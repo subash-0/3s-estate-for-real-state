2d1d3a11eb9909ac04cec0f4579bd9eb5c552010
subash-0 <subash67335@gmail.com>
2024-02-18 22:23:51 +0545

    listing show api created

--- api/db/controllers/user.controller.js
+++ api/db/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { errorHandler } from "../../security/error.js"
 import { hashPassword } from "../../security/passport.js"
+import listingModel from "../models/listing.model.js";
 import User from "../models/user.model.js";
 export const updateUser = async (req,res, next)=>{
    
@@ -36,4 +37,18 @@ export const deleteUser =async (req,res,next)=>{
     } catch (error) {
         next(error)
     }
+}
+
+export const getUserListing=async(req,res,next)=>{
+    if(req.user.id !== req.params.id) return next(errorHandler(401,'You can only view your own listing'));
+    try {
+
+      const listing = await  listingModel.find({userRef:req.params.id})
+      res.status(200).json(listing);
+        
+    } catch (error) {
+        next(error);
+        
+    }
+
 }
--- api/db/routers/user.router.js
+++ api/db/routers/user.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { updateUser,deleteUser } from "../controllers/user.controller.js";
+import { updateUser,deleteUser,getUserListing } from "../controllers/user.controller.js";
 import { verifyUser } from "../../security/verifyUser.js";
 const router = Router();
 router.put('/user/update/:id' ,verifyUser,updateUser);
 router.delete('/user/delete/:id' ,verifyUser,deleteUser);
+router.delete('/user/showlisting/:id' ,verifyUser,getUserListing);
 
 
 export default router;
