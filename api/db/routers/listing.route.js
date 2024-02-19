96be6f68d843e84d7e9bc9f5b191e4de404615b2
subash-0 <subash67335@gmail.com>
2024-02-19 15:29:59 +0545

    update api created

--- api/db/routers/listing.route.js
+++ api/db/routers/listing.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createListing,deleteListing } from "../controllers/listing.controller.js";
+import { createListing,deleteListing,updateListing } from "../controllers/listing.controller.js";
 import {verifyUser} from '../../security/verifyUser.js';
 const router = Router();
 router.post('/listing/create',verifyUser,createListing);
 router.delete('/listing/delete/:id',verifyUser,deleteListing);
+router.put('/listing/update/:id',verifyUser,updateListing);
 export default router;
